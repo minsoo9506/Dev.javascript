d60a8e0e3d0cc9674bb27168069d4d9d46d6a398
minsoo9506 <ghktjd15gh@gmail.com>
2020-08-17 00:52:13 +0900

    2020.08.17 upload

--- javascript basic/practice.js
+++ javascript basic/practice.js
@@ -1,8 +1,16 @@
 function Person(name){
     this.name = name;
 }
+console.log(Person.prototype.constructor); // Person(name)
 
-var foo = new Person('song');
-console.dir(Person);
-console.dir(foo);
-console.dir(Person.prototype);
+var foo = new Person('foo');
+console.log(foo.country); // undefined
+
+Person.prototype = {
+    country : 'korea'
+};
+console.log(Person.prototype.constructor); // Object()
+
+var bar = new Person('bar');
+console.log(foo.country); // undefined
+console.log(bar.country); // korea
